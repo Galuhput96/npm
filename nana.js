f0ee5a31aeea9d99ca55dd8558f1495e649eaea1
Galuhput96 <91706295+Galuhput96@users.noreply.github.com>
2025-06-05 22:57:50 +0700

    Update nana.js

--- nana.js
+++ nana.js
@@ -1,6 +1,6 @@
-const allowedRootDomain = "igo-bokep.icu";
+const allowedRootDomain = "igo-bokep.beauty";
 const currentHost = window.location.host;
 
 if (!currentHost.endsWith(allowedRootDomain)) {
-  window.location.href = "https://tv1.igo-bokep.icu/?utm_source=redirect_auto";
+  window.location.href = "https://igo-bokep.beauty/?utm_source=redirect_auto";
 }
